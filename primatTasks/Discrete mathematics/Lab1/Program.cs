2c835badf50ffd5b580f533bf88b9d37b7f6858e
Nikita <nikita.ushakov.02@outlook.com>
2023-03-02 23:29:02 +0200

    + docker

---
+++ primatTasks/Discrete mathematics/Lab1/Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Numerics;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("###############################");
+            Console.WriteLine("Exercise 1.");
+            Console.WriteLine("###############################\n");
+
+            AmountWords(); // 4.826.809
+
+            /*Console.WriteLine("\n###############################");
+            Console.WriteLine("Exercise 2.");
+            Console.WriteLine("###############################\n");
+
+            CountingRoads();
+
+            Console.WriteLine("\n###############################");
+            Console.WriteLine("Exercise 3.");
+            Console.WriteLine("###############################\n");
+
+            CountingRoadsAdditionalConditions();
+
+            Console.WriteLine("\n###############################");
+            Console.WriteLine("Exercise 4.");
+            Console.WriteLine("###############################\n");
+
+            int numberElement = 50;
+            Console.WriteLine("Рекуррентная формула: " + RecurrentFormula(numberElement));
+            Console.WriteLine("Общее решение: " + GeneralSolution(numberElement));*/
+        }
+
+        public static void AmountWords()
+        {
+            List<string> arr = new List<string> { "к", "о", "м", "б", "и", "н", "а", "т", "о", "р", "и", "к", "а" };
+
+            int length = arr.Count();
+            List<string> allWords = new List<string>();
+
+            for (int i = 0; i < length; i++)
+                for (int j = 0; j < length; j++)
+                    for (int k = 0; k < length; k++)
+                        for (int l = 0; l < length; l++)
+                            for (int m = 0; m < length; m++)
+                                for (int n = 0; n < length; n++)
+                                    allWords.Add($"{arr[i]}{arr[j]}{arr[k]}{arr[l]}{arr[m]}{arr[n]}");
+
+            Console.WriteLine(allWords.Count());
+        }
+
+        public static void CountingRoads()
+        {
+            int m = 17;
+            int n = 12;
+
+            int[,] matrix = new int [m, n];
+
+            for (int i = 0; i < m; i++)
+                for (int j = 0; j < n; j++)
+                    matrix[i, j] = 0;
+
+            matrix[0, 0] = 1;
+
+            for(int count = 1; count < m; count++)
+            {
+                int i = count;
+                int j = 0;
+                while(i >= 0 && j < n)
+                {
+                    if (i != 0)
+                        matrix[i, j] = matrix[i - 1, j];
+                    if (j != 0)
+                        matrix[i, j] += matrix[i, j - 1];
+
+                    i--;
+                    j++;
+
+                    if (i == 0 && j == 1)
+                        matrix[i, j] = 1;
+                }
+            }
+
+            for(int count = 1; count < n; count++)
+            {
+                int i = m - 1;
+                int j = count;
+
+                while(j < n && i >= 0)
+                {
+                    if (i != 0)
+                        matrix[i, j] = matrix[i - 1, j];
+                    if (j != 0)
+                        matrix[i, j] += matrix[i, j - 1];
+
+                    i--;
+                    j++;
+                }
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    Console.Write($"{matrix[i, j]}, ");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(matrix[m - 1, n - 1]);
+        }
+
+        public static void CountingRoadsAdditionalConditions()
+        {
+            int m = 17;
+            int n = 12;
+
+            int[,] matrix = new int[m, n];
+
+            for (int i = 0; i < m; i++)
+                for (int j = 0; j < n; j++)
+                    matrix[i, j] = 0;
+
+            matrix[0, 0] = 1;
+
+            for (int count = 1; count < m; count++)
+            {
+                int i = count;
+                int j = 0;
+                while (i >= 0 && j < n)
+                {
+                    if (i != 0)
+                        matrix[i, j] = matrix[i - 1, j];
+                    if (j != 0 && i != 0)
+                        matrix[i, j] += matrix[i - 1, j - 1];
+
+                    i--;
+                    j++;
+
+                    if (i == 0 && j == 1)
+                        matrix[i, j] = 1;
+                }
+            }
+
+            for (int count = 1; count < n; count++)
+            {
+                int i = m - 1;
+                int j = count;
+
+                while (j < n && i >= 0)
+                {
+                    if (i != 0)
+                        matrix[i, j] = matrix[i - 1, j];
+                    if (j != 0 && i != 0)
+                        matrix[i, j] += matrix[i - 1, j - 1];
+
+                    i--;
+                    j++;
+                }
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    Console.Write($"{matrix[i, j]}, ");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(matrix[m - 1, n - 1]);
+        }
+
+        public static int RecurrentFormula(int k)
+        {
+            if (k < 1)
+                return 1;
+            else if (k == 1)
+                return -7;
+            else
+            {
+                int result = 5 * RecurrentFormula(k - 1) + 6 * RecurrentFormula(k - 2);
+                return result;
+            } 
+        }
+        public static int GeneralSolution(int k)
+        {
+            double result = ((Math.Pow(13 * (-1), k) - Math.Pow(6, k + 1)) / 7);
+            int res = (int)Math.Round(result);
+            return res;
+        }
+    }
+}
---
+++ primatTasks/Discrete mathematics/Lab2/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Tracing;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+
+        static void DistanceHemminga()
+        {
+            char[] m = new char[] {' ', 'и', 'к', 'л', 'м', 'н', 'о', 'п', 'р'};
+            string[] n = new string[] { "00000", "00011", "00101", "00110", "01001", "01010", "10001", "11000" };
+            var table = new List<List<string>>[100];
+
+            for (int i = 0; i < n.Length; i++)
+            {
+                var line = new List<string>(m[i + 1]);
+
+                for (int l = 0; l < i; l++)
+                    line.Add("");
+
+                line.Add("-");
+
+                for (int j = i + 1; j < n.Length; j++)
+                {
+                    var num = 0;
+                    for (int k = 0; k < n[i].Length; k++)
+                        if (n[i][k] != n[j][k])
+                            num++;
+                    line.Add(num.ToString());
+                }
+                table.Add(line);
+            }
+            Console.WriteLine(tabulate(table, m, tablefmt = "psql", numalign = 'center'))
+        }
+
+        static void CodingRLE()
+        {
+            // aaaaaaaaaaaaadghtttttttttttyiklooooooop 
+
+            string input = "aaaaaaaaaaaaadghtttttttttttyiklooooooop";
+
+            int amount = 2;
+            string final_string = "";
+
+            for(int i = 0; i < input.Length - 1; i++)
+            {
+                if (input[i] == input[i + 1])
+                    amount++;
+                else
+                {
+                    var a = input[i];
+                    final_string = final_string + amount.ToString() + input[i]
+                        + " ";
+                    amount = 1;
+                }
+
+                
+            }
+
+            if(input[input.Length - 1] != input[input.Length - 2])
+                final_string += amount.ToString() + 
+                    input[input.Length - 1] + " ";
+
+            Console.WriteLine(final_string);
+        }
+
+        static void ArithmeticCoding()
+        {
+            string alphabet = "aecdfb";
+            float[] p = new float[] { 0.10f, 0.10f, 0.05f, 0.55f, 0.10f, 0.10f };
+            string word = "eacdbf";
+            var code = Coding(word, alphabet, p);
+            Console.WriteLine(code);
+            Console.WriteLine(Decoding(code, word.Length, alphabet, p));
+        }
+
+        static string Float2Bin(int x, double eps = 1e-9)
+        {
+            string res = "";
+            while(x > eps)
+            {
+                x *= 2;
+                res += (int)x;
+                x -= (int)x;
+            }
+
+            return res;
+        }
+
+        static int Bin2Float(int x)
+        {
+            var result = Math.Pow(2, (-1 * i - 1));
+            return result;
+        }
+
+        static string FindCode(string a, string b)
+        {
+            int i = 0;
+            a += '0' * (b.Length - a.Length);
+
+            while (a[i] == b[i])
+                i++;
+
+            var res = a[:i] +'0';
+            var cnt = 0;
+
+            while (a[i] == 1)
+            {
+                i++;
+                cnt++;
+            }
+            res += "1" * (cnt + 1);
+            return res;
+        }
+
+        static string Coding(string word, string certainChar, float[] p)
+        {
+            int left = 0;
+            int right = 1;
+
+            foreach(var letter in word)
+            {
+                var i = certainChar.IndexOf(letter);
+                left, right = (left + (left - right) * sum(p[:i]),
+                    left + (right - left) * sum(p[: i + 1]));
+            }
+
+            return FindCode(*map(Float2Bin, (left, right)));
+        }
+
+        static void Decoding(int code, int length, char alphabet, int p)
+        {
+            code = Bin2Float(code);
+            string word = "";
+
+            int left = 0;
+            int right = 1;
+
+            for(int j = 0; j < length; j++)
+                for(int i = 0; )
+        }
+    }
+}
